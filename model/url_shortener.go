496ba08d08cf0a3a2886f9121dc8db6e902a8c86
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-01-27 12:40:44 +0100

    Add example of expected JSON

--- model/url_shortener.go
+++ model/url_shortener.go
@@ -1,11 +1,20 @@
 package model
 
 // URLShortenerRequest represents request for URLShortenerHandler
+// JSON looks like:
+// {
+//   "url": "http://example.com/vvv/bbb/ccc/1212/xxx.html"
+// }
 type URLShortenerRequest struct {
 	URL string `json:"url"`
 }
 
 // URLShortenerResponse represents response for URLShortenerHandler
+// JSON looks like:
+// {
+//   "shortUrl": "http://sh.cc/wX2S1qz",
+//   "result": "success"
+// }
 type URLShortenerResponse struct {
 	ShortURL string `json:"shortUrl"`
 	Result   string `json:"result"`
