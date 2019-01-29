2690b9d7bc95e3e4211b6561f597b2809a8f53a5
Artem Nikitin <artemnikitin@artemnikitin.com>
2019-01-29 21:39:56 +0100

    Return back build of zip archives

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ for file in cmd/*.go; do
     echo "Building:" ${file##*/}
     name=$(echo "${file##*/}" | cut -f 1 -d '.')
     GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o build/${name} cmd/${file##*/}
+    zip build/${name##*/}.zip build/${name##*/}
 done
